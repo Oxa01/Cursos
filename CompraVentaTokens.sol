8852dab42669b66504890b0601e2f79149a6cff7
Oxa01 <100632158+Oxa01@users.noreply.github.com>
2022-05-11 00:57:47 -0500

    Update CompraVentaTokens.sol

--- CompraVentaTokens.sol
+++ CompraVentaTokens.sol
@@ -9,7 +9,7 @@ contract MyToken is ERC20 {
     uint256 FACTOR = 50;
     constructor() ERC20("MyToken", "MTK") {    
     }
-
+    // esta funcion mintea tokens a cambio de ETH
     function compraToken() public payable {
         payable(address(this)).transfer(msg.value);
         _mint(msg.sender, msg.value * FACTOR);
@@ -17,6 +17,7 @@ contract MyToken is ERC20 {
     function saldoContrato() public view returns(uint256) {
        return(address(this).balance);
     }
+    //esta funcion permite que los usuarios vendan tokens al contrato a cambio de ETH
     function ventaToken(uint256 cantidad) public {
         payable(msg.sender).transfer(cantidad * 10 ** decimals() / FACTOR);
         transfer(address(this),cantidad * 10 ** decimals());
